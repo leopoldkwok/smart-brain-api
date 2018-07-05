82b34b7d2901a793dd53318514f662bde41f481b
Leo <leo.kwok@beamly.com>
2018-07-05 14:10:17 +0100

    placing the smart brain api app in a respoistory

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+// You must add your own API key here from Calrifai
+const app = new Clarifai.App({
+    apiKey: 'e916240c41e2491db3d062f5c58e29ae'
+  });
+
+const handleApiCall = (req, res) => {
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
